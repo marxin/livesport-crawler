d3bd2badeb8e91152af97eee212694261250593d
Martin Liska <martin.liska@hey.com>
2024-07-30 14:14:33 +0200

    Add get_score function that does not panic

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
+use chrono::LocalResult::Single;
 use chrono::{DateTime, Datelike, Local, TimeZone};
+use fantoccini::Client;
 use fantoccini::{wd::Capabilities, ClientBuilder, Locator};
 use std::sync::mpsc::channel;
 use std::{
@@ -6,7 +8,7 @@ use std::{
     thread,
     time::Duration,
 };
-use tracing::info;
+use tracing::{info, warn};
 use url::Url;
 
 const DRIVER_PORT: u16 = 9515;
@@ -35,6 +37,94 @@ fn start_driver() -> anyhow::Result<Child> {
     Ok(driver)
 }
 
+async fn get_score(client: &mut Client) -> anyhow::Result<GameResult> {
+    let base_url = Url::parse(TEAM_URL)?;
+    client.goto(base_url.join("vysledky")?.as_str()).await?;
+
+    let match_rows = client.find_all(Locator::Css(".event__match")).await?;
+    let last_match_row = &match_rows
+        .first()
+        .ok_or(anyhow::anyhow!("could not find .event__match element"))?;
+
+    let home_team = last_match_row
+        .find(Locator::Css(".event__participant--home"))
+        .await?
+        .text()
+        .await?;
+
+    let away_team = last_match_row
+        .find(Locator::Css(".event__participant--away"))
+        .await?
+        .text()
+        .await?;
+
+    let home_score = last_match_row
+        .find(Locator::Css(".event__score--home"))
+        .await?
+        .text()
+        .await?
+        .parse::<u32>()?;
+
+    let away_score = last_match_row
+        .find(Locator::Css(".event__score--away"))
+        .await?
+        .text()
+        .await?
+        .parse::<u32>()?;
+
+    let event_time = last_match_row
+        .find(Locator::Css(".event__time"))
+        .await?
+        .text()
+        .await?;
+    let event_time = event_time
+        .split('\n')
+        .next()
+        .ok_or(anyhow::anyhow!(".event__time is empty"))?
+        .to_string();
+
+    // TODO: we assume the last match happened in the same year!
+    let event_time_parts = event_time
+        .split(['.', ' ', ':'])
+        .filter(|s| !s.is_empty())
+        .map(|x| x.parse::<u32>())
+        .collect::<Result<Vec<_>, _>>()?;
+
+    let now = Local::now();
+    // TODO: Properly map an error if the result is equal to LocalResult::Ambiguous!
+    let event_date = now.timezone().with_ymd_and_hms(
+        now.year(),
+        event_time_parts[1],
+        event_time_parts[0],
+        event_time_parts[2],
+        event_time_parts[3],
+        0,
+    );
+    let Single(event_date) = event_date else {
+        anyhow::bail!("ambigous or none date: {event_date:?}");
+    };
+
+    let latest_match = if home_team.starts_with(TEAM_NAME) {
+        GameResult {
+            my_team: home_team,
+            my_team_score: home_score,
+            opponent_team: away_team,
+            opponent_team_score: away_score,
+            event_date,
+        }
+    } else {
+        GameResult {
+            my_team: away_team,
+            my_team_score: away_score,
+            opponent_team: home_team,
+            opponent_team_score: home_score,
+            event_date,
+        }
+    };
+
+    Ok(latest_match)
+}
+
 // let's set up the sequence of steps we want the browser to take
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
@@ -49,99 +139,24 @@ async fn main() -> anyhow::Result<()> {
 
     let mut driver = start_driver()?;
 
-    let base_url = Url::parse(TEAM_URL)?;
     let cap: Capabilities =
         serde_json::from_str(r#"{"goog:chromeOptions":{"args":["--headless"]}}"#).unwrap();
 
-    let c = ClientBuilder::rustls()?
+    let mut c = ClientBuilder::rustls()?
         .capabilities(cap)
         .connect(&format!("http://localhost:{DRIVER_PORT}"))
         .await
         .expect("failed to connect to WebDriver");
 
     loop {
-        c.goto(base_url.join("vysledky")?.as_str()).await?;
-
-        let match_rows = c.find_all(Locator::Css(".event__match")).await?;
-        let last_match_row = &match_rows[0];
-
-        let home_team = last_match_row
-            .find(Locator::Css(".event__participant--home"))
-            .await?
-            .text()
-            .await?;
-
-        let away_team = last_match_row
-            .find(Locator::Css(".event__participant--away"))
-            .await?
-            .text()
-            .await?;
-
-        let home_score = last_match_row
-            .find(Locator::Css(".event__score--home"))
-            .await?
-            .text()
-            .await?
-            .parse::<u32>()?;
-
-        let away_score = last_match_row
-            .find(Locator::Css(".event__score--away"))
-            .await?
-            .text()
-            .await?
-            .parse::<u32>()?;
-
-        let event_time = last_match_row
-            .find(Locator::Css(".event__time"))
-            .await?
-            .text()
-            .await?;
-        let event_time = event_time
-            .split('\n')
-            .next()
-            .ok_or(anyhow::anyhow!(".event__time is empty"))?
-            .to_string();
-
-        // TODO: we assume the last match happened in the same year!
-        let event_time_parts = event_time
-            .split(['.', ' ', ':'])
-            .filter(|s| !s.is_empty())
-            .map(|x| x.parse::<u32>())
-            .collect::<Result<Vec<_>, _>>()?;
-
-        let now = Local::now();
-        // TODO: Properly map an error if the result is equal to LocalResult::Ambiguous!
-        let event_date = now
-            .timezone()
-            .with_ymd_and_hms(
-                now.year(),
-                event_time_parts[1],
-                event_time_parts[0],
-                event_time_parts[2],
-                event_time_parts[3],
-                0,
-            )
-            .unwrap();
-
-        let latest_match = if home_team.starts_with(TEAM_NAME) {
-            GameResult {
-                my_team: home_team,
-                my_team_score: home_score,
-                opponent_team: away_team,
-                opponent_team_score: away_score,
-                event_date,
+        match get_score(&mut c).await {
+            Ok(latest_match) => {
+                info!("latest match = {latest_match:?}");
             }
-        } else {
-            GameResult {
-                my_team: away_team,
-                my_team_score: away_score,
-                opponent_team: home_team,
-                opponent_team_score: home_score,
-                event_date,
+            Err(error) => {
+                warn!("got error: {error}");
             }
-        };
-
-        info!("latest match = {latest_match:?}");
+        }
 
         if rx.try_recv().is_ok() {
             info!("exitting the main loop");
